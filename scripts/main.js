99253c4597fa9037aa903ac5c2898e540a1d3445
Ayushkiller <malikayush999@gmail.com>
2024-01-31 17:56:27 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,7 @@ Events.run(Trigger.update, () => {
     Groups.build.each(b => {
       if(b.block != Blocks.graphitePress) return;
       b.items.set(Items.coal, 10);
+      if(b.block != Blocks.multi-press) return;
+      b.items.set(Items.coal, 10);
     })
   });
