82d3f38b642ed2291c36213634d661834df799b3
Ayushkiller <malikayush999@gmail.com>
2024-01-31 18:10:54 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,9 @@ Events.run(Trigger.update, () => {
   });
   Events.run(Trigger.update, () => {
     Groups.build.each(b => {
-      if(b.block != Blocks.multi-press) return;
+      if(b.block != Blocks.multiPress) return;
       b.items.set(Items.coal, 10);
+      if(b.block != Blocks.multiPress) return;
+      b.liquids.set(Liquids.water, 10);
     })
   });
